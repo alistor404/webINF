1a99fccc8e503f8a885e8386e0cd0eb9177500c3
alistor404 <foolcats@foxmail.com>
2016-07-18 23:09:20 +0800

    aaaa

--- config/routes.js
+++ config/routes.js
@@ -31,7 +31,7 @@ module.exports= function(app){
 		var dict = [nonce,timestamp,token];
 
 		var dict2=dict.sort().join('');
-		var Signture = crypto.createHmac("sha1", dict2).digest().toString('base64');
+		var Signture = crypto.createHmac("sha1").update(dict2).digest().toString('base64');
 		if( Signture==req.query.signature){
 			res.send(echostr)
 		}else{
